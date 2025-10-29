37ab2fe13b987c70854c51982c55e60529ec68c7
Дима <dimionik952@gmail.com>
2025-10-29 12:03:00 +0200

    Add function to write data to a text file

---
+++ main.py
@@ -0,0 +1,10 @@
+# Функція для запису даних у текстовий файл
+def write_to_file(filename):
+    try: 
+        with open(filename, "w") as f:
+            f.write("Образцов\n")
+            f.write("Питання: Як працює оператор try-except у Python?\n")
+    except Exception as e: print("Помилка під час роботи з файлом:", e)
+    else: print("Дані записано у файл.")
+
+write_to_file("python.txt")
